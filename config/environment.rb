608adc8b927c2b38ec0fe1641705945b4a316ff8
Emanuele Serafini <ema.serafini@gmail.com>
2017-05-19 21:14:13 +0200

    initial commit (hanami new sense --database postgresql --test rspec --application-name api)

---
+++ config/environment.rb
@@ -0,0 +1,49 @@
+require 'bundler/setup'
+require 'hanami/setup'
+require 'hanami/model'
+require_relative '../lib/sense'
+require_relative '../apps/api/application'
+
+Hanami.configure do
+  mount Api::Application, at: '/'
+
+  model do
+    ##
+    # Database adapter
+    #
+    # Available options:
+    #
+    #  * SQL adapter
+    #    adapter :sql, 'sqlite://db/sense_development.sqlite3'
+    #    adapter :sql, 'postgresql://localhost/sense_development'
+    #    adapter :sql, 'mysql://localhost/sense_development'
+    #
+    adapter :sql, ENV['DATABASE_URL']
+
+    ##
+    # Migrations
+    #
+    migrations 'db/migrations'
+    schema     'db/schema.sql'
+  end
+
+  mailer do
+    root 'lib/sense/mailers'
+
+    # See http://hanamirb.org/guides/mailers/delivery
+    delivery :test
+  end
+
+  environment :development do
+    # See: http://hanamirb.org/guides/projects/logging
+    logger level: :debug
+  end
+
+  environment :production do
+    logger level: :info, formatter: :json
+
+    mailer do
+      delivery :smtp, address: ENV['SMTP_HOST'], port: ENV['SMTP_PORT']
+    end
+  end
+end
---
+++ lib/sense.rb
@@ -0,0 +1,2 @@
+module Sense
+end
